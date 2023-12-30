64228f3d9c5370707d312d0725ac72e106669210
geetu040 <raoarmaghanshakir040@gmail.com>
2023-12-30 15:25:06 +0500

    converting GET to POST for contact service

--- microservices/contact/contact-function/index.js
+++ microservices/contact/contact-function/index.js
@@ -1,22 +1,24 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const email = req.query.email
-    const message = req.query.message
+    const email = req.body.email;
+    const message = req.body.message;
 
-    if (!email | !message) {
-        responseMessage = "Function is Ready";
+    if (!email || !message) {
+        context.res = {
+            status: 400,
+            body: "Bad Request: Email and message are required."
+        };
     } else {
         context.bindings.outputDocument = JSON.stringify({
             id: new Date().toISOString() + Math.random().toString().substring(2, 10),
             email: email,
             message: message
         });
-        responseMessage = "Successfully Added";
-    }
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
-}
+        context.res = {
+            status: 201, // 201 Created status for successful creation
+            body: "Successfully Added"
+        };
+    }
+};
