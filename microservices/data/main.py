04537f159a0e8fa950937c91869121295286f906
geetu040 <raoarmaghanshakir040@gmail.com>
2024-01-02 23:07:27 +0500

    allowed cors in data service

--- microservices/data/main.py
+++ microservices/data/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI, Query
-
+from fastapi.middleware.cors import CORSMiddleware
 from db import get_distinct_col, search
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/')
 def index():
